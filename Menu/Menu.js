a4760fe208c8e5c0463b9988d8318cead8f4fe0e
Rosine <lili_yang@hotmail.fr>
2024-02-22 16:49:29 +0100

    new file

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,5 +3,5 @@ const { redirect } = require("react-router-dom");
 document.getElementById('redirectionGame').addEventListener('click', redirect);
 
 function redirect() {
-    window.location('http://127.0.0.1:5500/Game.html');
+    window.location('Game/Game.html');
 }
